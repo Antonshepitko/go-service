ca4817b4af91d56e20a53862b12ee501bb3c7a42
Anton Shepitko <antonshepitko99@gmail.com>
2025-05-24 17:31:10 +0300

    prometheus

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
-import (
-    "fmt"
-    "log"
-    "net/http"
-)
+     import (
+         "fmt"
+         "log"
+         "net/http"
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    log.Printf("Received request from %s", r.RemoteAddr)
-    fmt.Fprintf(w, "Hello, World!")
-}
+         "github.com/prometheus/client_golang/prometheus"
+         "github.com/prometheus/client_golang/prometheus/promhttp"
+     )
 
-func main() {
-    http.HandleFunc("/", handler)
-    log.Println("Server starting on port 8080...")
-    http.ListenAndServe(":8080", nil)
-}
+     var (
+         requestCounter = prometheus.NewCounter(
+             prometheus.CounterOpts{
+                 Name: "http_requests_total",
+                 Help: "Total number of HTTP requests",
+             },
+         )
+     )
+
+     func init() {
+         prometheus.MustRegister(requestCounter)
+     }
+
+     func handler(w http.ResponseWriter, r *http.Request) {
+         requestCounter.Inc()
+         log.Printf("Received request from %s", r.RemoteAddr)
+         fmt.Fprintf(w, "Hello, World!")
+     }
+
+     func main() {
+         http.HandleFunc("/", handler)
+         http.Handle("/metrics", promhttp.Handler())
+         log.Println("Server starting on port 8080...")
+         http.ListenAndServe(":8080", nil)
+     }
